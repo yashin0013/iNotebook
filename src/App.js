a7de873ef545b7b62635f460b9ea3376f4720712
Yashin <yashin123786@gmail.com>
2023-04-02 08:30:36 +0530

    Router Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-   <div>
-    <h2>Test</h2>
-   </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
